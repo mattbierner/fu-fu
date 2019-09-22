e58781b20ad7a4496adaac7174a9f2ca5ab89007
Matt Bierner <mattbierner@gmail.com>
2019-09-22 11:20:49 -0700

    Updates

---
+++ precache-manifest.ae097122195a7b17d6016e98d5460eb3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "671b79402669a807073ac2f9b5f93059",
+    "url": "/fu-fu/index.html"
+  },
+  {
+    "revision": "69a064522efe99e5e863",
+    "url": "/fu-fu/static/css/main.098924c9.chunk.css"
+  },
+  {
+    "revision": "6e911d8ddd7715ff7382",
+    "url": "/fu-fu/static/js/2.b9d53070.chunk.js"
+  },
+  {
+    "revision": "69a064522efe99e5e863",
+    "url": "/fu-fu/static/js/main.1213bcaf.chunk.js"
+  },
+  {
+    "revision": "14eff0e99a6428972386",
+    "url": "/fu-fu/static/js/runtime-main.85aa0ed8.js"
+  },
+  {
+    "revision": "3bbc7bde66fdb3b9058fc4fb3f2f2f5b",
+    "url": "/fu-fu/static/media/logo.3bbc7bde.svg"
+  }
+]);
